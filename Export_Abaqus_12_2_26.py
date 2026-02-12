aeaabfca26745ad469e552689f323a5774731733
mshojaee3 <110466741+mshojaee3@users.noreply.github.com>
2026-02-12 16:32:05 +0100

    Comment out download function and example call
    Commented out the download function and example usage for clarity.

--- Export_Abaqus_12_2_26.py
+++ Export_Abaqus_12_2_26.py
@@ -1,6 +1,33 @@
 # -------------------------
 # Example call
 # -------------------------
+# RAW_BASE = "https://raw.githubusercontent.com/mshojaee3/AllFunctionsPub/main/"
+# def Download_py_lib_from_github(py_name):
+#     # folder of this script (fallback to cwd)
+#     try:
+#         d = os.path.dirname(os.path.abspath(__file__))
+#     except Exception:
+#         d = os.getcwd()
+#     # save locally with spaces removed (import-friendly)
+#     local_name = py_name.replace(' ', '')
+#     local_path = os.path.join(d, local_name)
+#     if os.path.isfile(local_path):
+#         return local_path
+#     url = RAW_BASE + py_name.replace(' ', '%20')
+#     try:
+#         try:
+#             from urllib.request import urlopen  # Py3
+#         except ImportError:
+#             from urllib2 import urlopen         # Py2
+#         data = urlopen(url).read()
+#         with open(local_path, 'wb') as f:
+#             f.write(data)
+#         return local_path
+#     except Exception as e:
+#         print("Could not download:", url, "|", str(e))
+#         return None
+
+
 #Download_py_lib_from_github("Export_Abaqus_2_12_2026.py")
 #from Export_Abaqus_2_12_2026 import Export_Abaqus2CSV
 #exp = Export_Abaqus2CSV(JOB + '.odb', out_prefix=JOB, step_name=STEP, frame_index=-1)
