be247f11525af5e6f9be8f9f3b270a6d582726dd
mshojaee3 <110466741+mshojaee3@users.noreply.github.com>
2026-02-12 12:11:28 +0100

    Add script to download Python libraries from GitHub

---
+++ Download_py_lib_from_github.py
@@ -0,0 +1,26 @@
+RAW_BASE = "https://raw.githubusercontent.com/mshojaee3/AllFunctionsPub/main/"
+def Download_py_lib_from_github(py_name):
+    # folder of this script (fallback to cwd)
+    try:
+        d = os.path.dirname(os.path.abspath(__file__))
+    except Exception:
+        d = os.getcwd()
+    # save locally with spaces removed (import-friendly)
+    local_name = py_name.replace(' ', '')
+    local_path = os.path.join(d, local_name)
+    if os.path.isfile(local_path):
+        return local_path
+    url = RAW_BASE + py_name.replace(' ', '%20')
+    try:
+        try:
+            from urllib.request import urlopen  # Py3
+        except ImportError:
+            from urllib2 import urlopen         # Py2
+        data = urlopen(url).read()
+        with open(local_path, 'wb') as f:
+            f.write(data)
+        return local_path
+    except Exception as e:
+        print("Could not download:", url, "|", str(e))
+        return None
+
